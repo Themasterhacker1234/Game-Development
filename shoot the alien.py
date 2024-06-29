ccb2f3047c1eb7ffc1fdfe95832afe4ece357516
Themasterhacker1234 <hani.alhadi.ha@gmail.com>
2024-06-29 19:52:41 +0300

    Add files via upload
    this is my second game it is an alien shooting game. hope you have fun!

---
+++ shoot the alien.py
@@ -0,0 +1,32 @@
+import pgzrun
+from random import randint
+
+TITLE="Doom"
+WIDTH=800
+HEIGHT=700
+
+
+message=""
+
+alien=Actor('alien')
+
+
+def draw():
+    
+    screen.clear()
+    screen.fill(color=(128,0,0))
+
+    alien.draw()
+    screen.draw.text(message, center=(400,10), fontsize=30)
+def place_alien():
+    alien.x = randint(50, WIDTH-50)
+    alien.y = randint(50, HEIGHT-50)
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message="Good Shot"
+        place_alien()
+    else:
+        message="You Missed"
+place_alien()
+pgzrun.go()
