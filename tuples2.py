8e9597e01fcc8986cc8b5d15fa029082b559c425
Themasterhacker1234 <hani.alhadi.ha@gmail.com>
2024-07-29 18:33:56 +0300

    Add files via upload

---
+++ tuples2.py
@@ -0,0 +1,11 @@
+StuDetails=('Surabhi',89)
+address=('227','Brickfield Shelters','Bangalore','Karnataka','562107')
+for x in address:
+    print(x,end=' ')
+houseno,ApartName,city,state,pin=address
+print()
+print("HNO",houseno)
+print('APT NO',ApartName)
+print(city)
+print(state)
+print(pin)
